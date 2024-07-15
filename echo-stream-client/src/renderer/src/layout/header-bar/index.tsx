42e9ec21432bbc02f6c7b12bff48955c8f82aa63
zhi.jiang <zhi.jiang@sectrend.com.cn>
2024-07-15 15:02:22 +0800

    feat: 修改uno配置

--- echo-stream-client/src/renderer/src/layout/header-bar/index.tsx
+++ echo-stream-client/src/renderer/src/layout/header-bar/index.tsx
@@ -1,6 +1,6 @@
 export function HeaderBar(): JSX.Element {
   return (
-    <header className="w-screen flex flex-row h-10 bg-neutral-800">
+    <header className="w-screen flex flex-row h-40 bg-neutral-800">
       <div className="w-52">233</div>
       <div>322</div>
     </header>
--- echo-stream-client/uno.config.ts
+++ echo-stream-client/uno.config.ts
@@ -1,9 +1,16 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
-//import presetRemToPx from '@unocss/preset-rem-to-px'
+import { defineConfig, presetUno, presetAttributify } from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
 
 export default defineConfig({
-  // presets: [presetRemToPx()],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetRemToPx({
+      //默认情况下（1单位 = 0.25rem）html默认字体是16, 改为4, 每单位就是1px
+      baseFontSize: 4
+    })
+  ],
   theme: {
     colors: {
       veryCool: '#0000ff'
