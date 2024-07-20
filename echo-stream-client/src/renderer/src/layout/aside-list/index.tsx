f59c175ae59602bce631afe3ff2c5bd6a01a0907
aa1213889 <869135421@qq.com>
2024-07-20 23:27:00 +0800

    添加图标

--- echo-stream-client/src/renderer/src/layout/aside-list/index.tsx
+++ echo-stream-client/src/renderer/src/layout/aside-list/index.tsx
@@ -1,4 +1,5 @@
-import { UserOutlined } from '@ant-design/icons'
+import { DownloadOutlined, HeartOutlined, UserOutlined } from '@ant-design/icons'
+import { DailyIcon, MusicIcon, AddIcon, MusicListIcon, RecentlyPlayedIcon } from '@icons/'
 import { Avatar } from 'antd'
 
 export function AsideList(): JSX.Element {
@@ -8,21 +9,47 @@ export function AsideList(): JSX.Element {
         <Avatar size={32} icon={<UserOutlined />} />
         <span className="text-14 font-bold">用户名</span>
       </section>
-      <ul className="px-16 mt-16 text-14 font-bold">
-        <li className="mb-12">发现音乐</li>
-        <li>每日推荐</li>
+      <ul className="mt-16 text-14 font-bold px-0">
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <MusicIcon className="mr-8 " />
+          发现音乐
+        </li>
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <DailyIcon className="mr-8" />
+          每日推荐
+        </li>
       </ul>
-      <ul className="px-16 mt-16 text-14">
-        <li className="mb-6 text-12 text-neutral-500">我的音乐</li>
-        <li className="mb-12">我喜欢</li>
-        <li className="mb-12">最近播放</li>
-        <li>下载管理</li>
+      <ul className="mt-16 text-14 px-0">
+        <li className="pl-16 mb-6 text-12 text-neutral-500">我的音乐</li>
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <HeartOutlined className="mr-8" />
+          我喜欢
+        </li>
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <RecentlyPlayedIcon className="mr-8" />
+          最近播放
+        </li>
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <DownloadOutlined className="mr-8" />
+          下载管理
+        </li>
       </ul>
-      <ul className="px-16 mt-16 text-14">
-        <li className="mb-6 text-12 text-neutral-500">创建的歌单</li>
-        <li className="mb-12">歌单1</li>
-        <li className="mb-12">歌单12</li>
-        <li>歌单1</li>
+      <ul className="px-0 mt-16 text-14">
+        <li className="pl-16 mb-6 text-12 text-neutral-500 flex justify-between">
+          创建的歌单 <AddIcon className="mr-8 cursor-pointer" />
+        </li>
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <MusicListIcon className="mr-8" />
+          歌单1
+        </li>
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <MusicListIcon className="mr-8" />
+          歌单1
+        </li>
+        <li className="pl-16 py-4 hover:bg-neutral-950 transition-background duration-300 ease-in-ou cursor-pointer">
+          <MusicListIcon className="mr-8" />
+          歌单1
+        </li>
       </ul>
     </aside>
   )
