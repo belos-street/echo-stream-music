2e8d2f051fc92e2ad02a3387efb11754665691d4
zhi.jiang <zhi.jiang@sectrend.com.cn>
2024-09-12 17:54:23 +0800

    feat: 修改dto

--- echo-stream-server/src/song/dto/mark.dto.ts
+++ echo-stream-server/src/song/dto/mark.dto.ts
@@ -1,3 +1,9 @@
+import { IsNotEmpty } from 'class-validator'
+
 export class MarkFavoriteDto {
+  @IsNotEmpty({ message: '歌曲ID不能为空' })
   songId: number
+
+  @IsNotEmpty({ message: '用户ID不能为空' })
+  userId: number
 }
