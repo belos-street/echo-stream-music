21475eb688f43c765cecf4ca94402276a1f3a1aa
zhi.jiang <zhi.jiang@sectrend.com.cn>
2024-09-18 16:06:25 +0800

    feat

--- echo-stream-client/src/renderer/src/App.tsx
+++ echo-stream-client/src/renderer/src/App.tsx
@@ -3,19 +3,9 @@ import { AsideList } from './layout/aside-list'
 import { HeaderBar } from './layout/header-bar'
 import { MusicBar } from './layout/music-bar'
 import { MusicList } from './page/music-list'
-import { Button } from 'antd'
-import { userInfoRequest } from './server/api/user'
 
 import { Daily } from './page/daily'
-
-//继续封装请求器
-function clickTest() {
-  userInfoRequest({
-    id: 1
-  }).then((res) => {
-    console.log(res, 'okkkkkkk')
-  })
-}
+import { Like } from './page/like'
 
 function App(): JSX.Element {
   return (
@@ -27,13 +17,12 @@ function App(): JSX.Element {
           <Routes>
             <Route path="/" element={<h1>111</h1>} />
             <Route path="/daily" element={<Daily />} />
-            <Route path="/like" element={<h1>333</h1>} />
+            <Route path="/like" element={<Like />} />
             <Route path="/recently" element={<h1>444</h1>} />
             <Route path="/download" element={<h1>555</h1>} />
             <Route path="/list/:id" element={<MusicList />} />
           </Routes>
         </main>
-        <Button onClick={() => clickTest()}>click test</Button>
       </div>
       <MusicBar />
     </>
---
+++ echo-stream-client/src/renderer/src/page/like/index.tsx
@@ -0,0 +1,13 @@
+export function Like(): JSX.Element {
+  return (
+    <div className="flex flex-col">
+      <div className="flex p-5 gap-5">
+        <div className="flex-1">1</div>
+        <div className="flex-1">
+          <div>我喜欢的音乐</div>
+        </div>
+      </div>
+      <div>2</div>
+    </div>
+  )
+}
